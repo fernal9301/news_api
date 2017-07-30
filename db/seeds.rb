01f05d5359ae877e152972194c9a3caf06d7f1ce
Sergey Ivanov <ivanovsergey1988@gmail.com>
2017-07-30 10:53:27 +0000

    add data to db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(email: 'u1@mail.ru', password: 'abc123')
+user2 = User.create(email: 'u2@mail.ru', password: 'abc123')
+user3 = User.create(email: 'u3@mail.ru', password: 'abc123')
+
+news1 = user1.news.create(title: 'title1', content: 'content1')
+news2 = user2.news.create(title: 'title2', content: 'content2')
+news3 = user3.news.create(title: 'title3', content: 'content3')
